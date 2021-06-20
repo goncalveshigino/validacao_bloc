f756312cbc0dfa77219f291ed45e491287e71383
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-20 09:36:36 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/validation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.validation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/pages/login/login_page.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        _createBackground( context),
+        _loginForm(context),
+      ],
+    ));
+  }
+
+
+  _loginForm( BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+     return SingleChildScrollView(
+       child: Column(
+         children: [
+
+          SafeArea(
+            child: Container(
+              height: 180.0,
+            ),
+          ),
+
+
+           Container(
+            width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 50.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow> [
+                 BoxShadow(
+                   color: Colors.black26, 
+                   blurRadius: 3.0, 
+                   offset: Offset(0.0, 5.0), 
+                   spreadRadius: 3.0
+                 )
+              ]
+            ),
+
+            child: Column(
+              children: [
+                Text('Entrar', style: TextStyle(fontSize:  20.0)),
+                SizedBox(height: 30.0,),
+                _creatEmail(), 
+                SizedBox(height: 30.0,),
+                _creatPassword(),
+               SizedBox(height: 30.0,),
+                _createButton()
+              ],
+            ),
+           ), 
+
+            TextButton(
+              child: Text('Esqueceu a senha', style: TextStyle(color: Colors.deepPurple),),
+              onPressed: (){},
+            ),
+
+            SizedBox(height: 100.0,)
+         ],
+
+       ),
+     );
+  }
+
+  _creatEmail(){
+
+    return Container(
+
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon( Icons.alternate_email, color: Colors.deepPurple),
+          hintText: 'Seu email',
+          labelText: 'Email'
+        ),
+      ),
+    );
+  }
+
+  _createButton(){ 
+
+    return RaisedButton(
+
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Entrar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      elevation: 5.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){
+        print('foiii');
+      },
+    );
+  }
+
+   _creatPassword(){
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          icon: Icon( Icons.lock_outline, color: Colors.deepPurple),
+          labelText: 'Password'
+        ),
+      ),
+    );
+  }
+
+
+  _createBackground( BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+    final background = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color> [
+            Color.fromRGBO(63, 63, 156, 1.0),
+            Color.fromRGBO(90, 70, 178,1.0)
+          ]
+        )
+      ),
+    );
+
+
+    final circle = Container(
+
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255,0.05)
+      ),
+    );
+
+
+    return Stack(
+      children: [
+        background,
+        
+        Positioned( top: 80.0, left: 20.0, child: circle,),
+        Positioned( top: -40.0, right: -30.0, child: circle,), 
+        Positioned( top: 50.0, right: 40.0, child: circle,), 
+
+
+        Container(
+          padding: const EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: [
+              Icon( Icons.person_pin_circle, color: Colors.white, size: 100.0),
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text('Goncalves Higino', style: TextStyle( color: Colors.white, fontSize: 25.0))
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+
+
+
+}
