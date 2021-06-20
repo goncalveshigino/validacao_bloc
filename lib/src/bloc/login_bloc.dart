b17ddf2b73f45c8af59c1064f49a85c246c911fc
goncalveshigino <goncalveshigino20@gmail.com>
2021-06-20 12:39:03 +0100

    bloc

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -1,29 +1,37 @@
-
-
-
 import 'dart:async';
-
 import 'package:validation/src/bloc/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
-class LoginBloc  with Validators {
+class LoginBloc with Validators {
 
-  final _emailController    = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
 
-  // Recuperar a saida da Stream 
- Stream<String> get emailStream    => _emailController.stream.transform( validarEmail);
- Stream<String> get passwordStream => _passwordController.stream.transform( validarPassword);
-
-   // Inserir valores na Stream
-  Function(String) get changeEmail      =>  _emailController.sink.add;
-  Function(String) get changePassword   =>  _passwordController.sink.add;
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
 
 
- dispose() { 
+  // Recuperar a saida da Stream
+  Stream<String> get emailStream =>
+      _emailController.stream.transform(validarEmail);
+  Stream<String> get passwordStream =>
+      _passwordController.stream.transform(validarPassword);
+
+  Stream<bool> get formValidStream =>
+      Observable.combineLatest2(emailStream, passwordStream, (e, p) => true);
+
+  // Inserir valores na Stream
+    Function(String) get changeEmail => _emailController.sink.add;
+    Function(String) get changePassword => _passwordController.sink.add;
+
+
+  //Obter ultimo valor inserido na stream
+    String get email     =>  _emailController.value;
+    String get password  =>  _passwordController.value;
+
+    
+
+  dispose() {
     _emailController?.close();
     _passwordController?.close();
   }
-
-
 }
